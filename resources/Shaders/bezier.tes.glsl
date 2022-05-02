e0f5f1cb5f9ba4960f2c2311d394e2a166430a8b
Kevin Pruvost <kevin.pruvost@epitech.eu>
2022-05-02 14:25:16 +0200

    Update bezier.tes.glsl

--- resources/Shaders/bezier.tes.glsl
+++ resources/Shaders/bezier.tes.glsl
@@ -7,8 +7,10 @@ out vec2 TexCoord;
 void main()
 {
     vec4 p[5][5];
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
             p[j][i] = gl_in[i * 5 + j].gl_Position;
         }
     }
@@ -17,26 +19,25 @@ void main()
     float v = gl_TessCoord.y;
     TexCoord = vec2(u, v);
 
-    // Computing the Position, given a u and v
-	// the basis functions:
     float bu[5];
-    bu[0] = (1.-u) * (1.-u) * (1.-u) * (1.-u);
-    bu[1] = 4. * u * (1.-u) * (1.-u) * (1.-u);
-    bu[2] = 6. * u * u * (1.-u) * (1.-u);
-    bu[3] = 4. * u * u * u * (1.-u);
-    bu[4] = u * u * u * u;
+    bu[0] = pow((1.0f-u), 4);
+    bu[1] = 4.0f * u * pow((1.0f-u), 3);
+    bu[2] = 6.0f * pow(u, 2) * pow((1.0f-u), 2);
+    bu[3] = 4.0f * pow(u, 3) * (1.0f-u);
+    bu[4] = pow(u, 4);
 
     float bv[5];
-    bv[0] = (1.-v) * (1.-v) * (1.-v) * (1.-v);
-    bv[1] = 4. * v * (1.-v) * (1.-v) * (1.-v);
-    bv[2] = 6. * v * v * (1.-v) * (1.-v);
-    bv[3] = 4. * v * v * v * (1.-v);
-    bv[4] = v * v * v * v;
+    bv[0] = pow((1.0f-v), 4);
+    bv[1] = 4.0f * v * pow((1.0f-v), 3);
+    bv[2] = 6.0f * pow(v, 2) * pow((1.0f-v), 2);
+    bv[3] = 4.0f * pow(v, 3) * (1.0f-v);
+    bv[4] = pow(v, 4);
 
-    // finally, we get to compute something:
-    vec4 res = vec4(0., 0., 0., 0.);
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    vec4 res = vec4(0.0f, 0.0f, 0.0f, 0.0f);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
             res += bu[i] * bv[j] * p[i][j];
         }
     }
